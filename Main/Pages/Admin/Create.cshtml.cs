17e2422285128389352eab1febf2cb01008bfb4c
Artem Kurta <artemdog386@gmail.com>
2025-06-01 23:14:33 +0300

    create main tree structure

---
+++ Main/Pages/Admin/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Main.Pages.Admin
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
